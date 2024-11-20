17d1eefff2dc34f9ce6a82f88431620a54baa709
Cloud User <username1@vm01-main.0reihd1fsqre3f4253cqoxtqvb.rx.internal.cloudapp.net>
2024-11-20 05:05:08 +0000

    updated main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 # Resource Group
 resource "azurerm_resource_group" "rg_new" {
   name     = "RG-SC3"        # Updated resource group name
-  location = "Central India"
+  location = "Central Canada"
 }
 
 # Virtual Network
