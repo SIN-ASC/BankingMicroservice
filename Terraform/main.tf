6a232dfc6a8921bdfcbf4c050387d541d86fca78
Cloud User <username1@vm01-main.0reihd1fsqre3f4253cqoxtqvb.rx.internal.cloudapp.net>
2024-11-20 05:56:26 +0000

    updated main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,103 +1,3 @@
-# provider "azurerm" {
-#   features {}
-#   subscription_id = "0f519cc7-9081-446a-9220-3cbc54c8d404"
-#   tenant_id = "4657bbb2-e4ed-4c25-ae0c-524e6d0d8061"
-# }
-
-# # Resource Group
-# resource "azurerm_resource_group" "rg_new" {
-#   name     = "RG-SC3"        # Updated resource group name
-#   location = "Canada Central"
-# }
-
-# # Virtual Network
-# resource "azurerm_virtual_network" "vnet_new" {
-#   name                = "vnet-SC3"  # Updated virtual network name
-#   address_space       = ["10.1.0.0/16"] # Updated address space
-#   location            = azurerm_resource_group.rg_new.location
-#   resource_group_name = azurerm_resource_group.rg_new.name
-# }
-
-# # Subnet
-# resource "azurerm_subnet" "subnet_new" {
-#   name                 = "subnet-SC3"    # Updated subnet name
-#   resource_group_name  = azurerm_resource_group.rg_new.name
-#   virtual_network_name = azurerm_virtual_network.vnet_new.name
-#   address_prefixes     = ["10.1.0.0/24"]     # Updated subnet address prefix
-# }
-
-# # Network Security Group
-# resource "azurerm_network_security_group" "nsg_new" {
-#   name                = "nsg-SC3"      # Updated NSG name
-#   location            = azurerm_resource_group.rg_new.location
-#   resource_group_name = azurerm_resource_group.rg_new.name
-# }
-
-# # Public IP Address
-# resource "azurerm_public_ip" "vm_public_ip_new" {
-#   name                = "vm-public-ip-SC3"  # Updated Public IP name
-#   location            = azurerm_resource_group.rg_new.location
-#   resource_group_name = azurerm_resource_group.rg_new.name
-#   allocation_method   = "Static"
-#   sku                  = "Standard"
-#   domain_name_label   = "myvm-public-ip-new-xyz"  # Updated domain name label
-# }
-
-# # Network Interface for Linux VM
-# resource "azurerm_network_interface" "nic_linux_new" {
-#   name                = "nic-linux-vm-new-xyz"   # Updated NIC name
-#   location            = azurerm_resource_group.rg_new.location
-#   resource_group_name = azurerm_resource_group.rg_new.name
-
-#   ip_configuration {
-#     name                          = "ipconfig-linux-new-xyz"   # Updated IP configuration name
-#     subnet_id                     = azurerm_subnet.subnet_new.id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = azurerm_public_ip.vm_public_ip_new.id  # Associate Public IP
-#   }
-# }
-
-# # Linux Virtual Machine
-# resource "azurerm_linux_virtual_machine" "linux_vm_new" {
-#   name                            = "linux-vm-SC3"      # Updated VM name
-#   resource_group_name             = azurerm_resource_group.rg_new.name
-#   location                        = azurerm_resource_group.rg_new.location
-#   size                            = "Standard_DS2_v2"       # Updated VM size
-#   admin_username                  = "username1"         # Keep the same username
-#   admin_password                  = "Password@123"          # Keep the same password
-#   disable_password_authentication = false
-
-#   network_interface_ids = [azurerm_network_interface.nic_linux_new.id]
-
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Premium_LRS"  # Updated disk storage type
-#   }
-
-#   source_image_reference {
-#     publisher = "solvedevops1643693563360"
-#     offer     = "rocky-linux-9"
-#     sku       = "plan001"
-#     version   = "latest"
-#   }
-
-#   plan {
-#     name      = "plan001"
-#     publisher = "solvedevops1643693563360"
-#     product   = "rocky-linux-9"
-#   }
-
-#   custom_data = base64encode(file("user-data.sh"))  # Base64 encode the user-data script for cloud-init
-# }
-
-# # Output Public IP Address
-# output "vm_public_ip_new" {
-#   value = azurerm_public_ip.vm_public_ip_new.ip_address
-# }
-
-
-
-
 provider "azurerm" {
   features {}
   subscription_id = "0f519cc7-9081-446a-9220-3cbc54c8d404"
@@ -249,3 +149,8 @@ EOT
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_public_ip.ip_address
 }
+
+
+# If you haven't accepted the legal terms on this subscription and getting error. Run the below commands:
+# az vm image accept-terms --offer rocky-linux-9 --publisher solvedevops1643693563360 --sku plan001
+# az vm image accept-terms --urn solvedevops1643693563360:rocky-linux-9:plan001:latest 
