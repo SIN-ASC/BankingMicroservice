ccfe0f36ecc9553426715638714f39cc7c4a8307
Cloud User <username1@vm01-main.0reihd1fsqre3f4253cqoxtqvb.rx.internal.cloudapp.net>
2024-11-21 02:39:47 +0000

    added vars.tf and terraform.tfvars

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg2.name
-  virtual_network_name = var.vnet_name
+  virtual_network_name = azurerm_virtual_network.vnet
   address_prefixes     = var.subnet_address_prefix
 }
  
